e7de5b98176ce3f655621163530921f8d0e06bf8
mat <mateusz.krzeszowiec@protonmail.com>
2019-04-05 00:03:36 +0200

    Added proper db script that'll create tables and add  test data used in the exploit

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,4 +1,17 @@
-CREATE TABLE `visitors` (
+CREATE TABLE IF NOT EXISTS `users` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL
+) DEFAULT CHARSET=utf8;
+
+INSERT INTO `users` (`username`, `password`) VALUES
+	('admin', 'who stores plaintext passwords in database these days?'),
+	('mat', 'very compelex password that\'s extremely hard to remember');
+
+CREATE TABLE IF NOT EXISTS `visitors` (
   `timestamp` datetime DEFAULT NULL,
   `name` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) DEFAULT CHARSET=utf8;
+
+INSERT INTO `visitors` (`timestamp`, `name`) VALUES
+	('2019-04-04 23:36:56', 'Summer Smith'),
+	('2019-04-04 23:37:00', 'Morty Smith');
